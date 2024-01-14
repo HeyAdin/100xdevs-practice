f9ffef7ef571925ac90e43f125d71ed0689b4a59
HeyAdin <adinahmad003@gmail.com>
2024-01-14 20:59:06 +0530

    import zod

--- week-3/class-3.1/authZod.js
+++ week-3/class-3.1/authZod.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// import zod 
+const z = require(zod);
+
 app.get('/login',(req,res)=>{
     res.send();
 });
 
 app.listen(PORT,()=>{
     console.log('Server Started');
-})
+});
