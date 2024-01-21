5db8c308819b761dc885b7cefca7a407d812b3bd
HeyAdin <adinahmad003@gmail.com>
2024-01-21 21:14:02 +0530

    Install cors package

--- week-4/class-4.1/sum.js
+++ week-4/class-4.1/sum.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
+app.use(cors());
 
 app.get('/sum', (req , res)=>{
     const num1 = +req.query.num1;
     const num2 = +req.query.num2;
     const result = num1+num2;
-    return result;
+    res.send(`Sum of ${num1} and ${num2} is = ${result}`);
+    // return result;
 });
 
 app.listen(PORT , ()=>{
