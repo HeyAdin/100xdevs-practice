dc91633a969fc6469b59e8932f9044514d1128a7
HeyAdin <adinahmad003@gmail.com>
2024-01-21 17:14:14 +0530

    sum function

--- week-4/class-4.1/sum.js
+++ week-4/class-4.1/sum.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 3000;
 
 app.get('/sum', (req , res)=>{
-
+    const num1 = +req.query.num1;
+    const num2 = +req.query.num2;
+    const result = num1+num2;
+    return result;
 });
 
 app.listen(PORT , ()=>{
