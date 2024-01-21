b3f49775841fc5b30df05ec39e31e38c12b2efe2
HeyAdin <adinahmad003@gmail.com>
2024-01-21 16:52:13 +0530

    Setup Express code

---
+++ week-4/class-4.1/sum.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/sum', (req , res)=>{
+
+});
+
+app.listen(PORT , ()=>{
+    console.log("Server Started");
+});
