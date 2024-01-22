853909de8e9ff302568e3da31cf6f9acffec2893
HeyAdin <adinahmad003@gmail.com>
2024-01-22 16:11:44 +0530

    Calculate Simple Interest

--- week-4/class-4.1/sum.js
+++ week-4/class-4.1/sum.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require("cors");
-app.use(cors());
+app.use(cors(),express.json());
 
 app.get('/sum', (req , res)=>{
     const num1 = +req.query.num1;
@@ -10,6 +10,18 @@ app.get('/sum', (req , res)=>{
     const result = num1+num2;
     res.send(`Sum of ${num1} and ${num2} is = ${result}`);
     // return result;
+});
+
+app.get('/interest',(req , res)=>{
+    const principle = +req.query.principle;
+    const time = +req.query.time;
+    const rate = +req.query.roi;
+    const interest = (principle*rate*time)/100;
+    const totalAmount = interest+principle;
+    res.json({
+        totalAmount,
+        interest
+    });
 });
 
 app.listen(PORT , ()=>{
