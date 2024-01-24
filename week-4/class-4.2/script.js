2e41f8177e92b0a19f8919ba8109f4c648dc11f9
HeyAdin <adinahmad003@gmail.com>
2024-01-24 14:28:55 +0530

    A simple todo app

---
+++ week-4/class-4.2/script.js
@@ -0,0 +1,22 @@
+function todo() {
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
+
+    const  container = document.querySelector("#container");
+    const div = document.createElement("div");
+    div.setAttribute('id',`${title}`)
+    
+    const todoTitle = document.createElement("h1");
+    todoTitle.textContent=title;
+    div.appendChild(todoTitle);
+
+    const todoDescription = document.createElement("p");
+    todoDescription.textContent=description;
+    div.appendChild(todoDescription);
+
+    const markBtn = document.createElement("button");
+    markBtn.textContent = "Mark as Done";
+    div.appendChild(markBtn);
+
+    container.appendChild(div);
+}
